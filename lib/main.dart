e00544b751be45a4ea659e799befe6b56e9d9f7a
TasneemDosoqi <46675287+TasneemDosoqi@users.noreply.github.com>
2020-08-14 21:23:01 +0300

    UI Enhancement

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,9 @@ class Quizzler extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Color(0xff5D72C5),
         body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
+          child: QuizPage(),
         ),
       ),
     );
@@ -32,7 +29,7 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
+          flex: 3,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
@@ -48,45 +45,92 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+          flex: 2,
+          child: CustomPaint(
+            painter: CurvePainter(),
+            child: Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(500),
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Color(0xff6AEB6A),
+                      child: Text(
+                        'True',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        //The user picked true.
+                      },
+                    ),
+                  ),
                 ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(500),
+                          ),
+                      ),
+                      color: Color(0xffEB6A70),
+                      child: Text(
+                        'False',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        //The user picked false.
+                      },
+                    ),
+                  ),
+                ),
+              ],
+              //TODO: Add a Row here as your score keeper
+            ),
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        //TODO: Add a Row here as your score keeper
       ],
     );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Color(0xffE7E9EB);
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.3);
+    path.quadraticBezierTo(size.width * 0.5, size.height * -0.48,
+        size.width * 1.0, size.height * 0.3);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
@@ -95,3 +139,8 @@ question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
+
+//path.quadraticBezierTo(size.width / 4, size.height,
+//size.width / 2, size.height);
+//path.quadraticBezierTo(size.width - (size.width / 4), size.height,
+//size.width, size.height - 40);
